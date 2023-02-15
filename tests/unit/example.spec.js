957b74038d358d7f2d3671fc2ce5000fa7f680ed
MageshRamadass <magesh.r@zebuetrade.com>
2023-02-15 17:38:41 +0530

    coma

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@./../../components/Layout/LayOut.vue'
+// import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@./../../components/Layout/LayOut.vue'
 
-describe('LayOut.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+// describe('LayOut.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
